8cf95e9f63d42fa5c129be9ad3fd242641a43209
Serhii Borsuk <serhiiborsuk@gmail.com>
2021-10-19 15:55:53 +0100

    add close button for erro popup

--- src/components/popups/error/ErrorPopup.tsx
+++ src/components/popups/error/ErrorPopup.tsx
@@ -4,8 +4,14 @@ import '../CommonPopup.css';
 
 export const ErrorPopup = (props: { isShown: boolean }): JSX.Element => {
     const { isShown } = props;
+    const [show, setIsShow] = useState(isShown);
+    useEffect(() => {
+       setIsShow(isShown);
+    }, [props]);
+
     return (
-        <div className={classNames('popup', { show: isShown })}>
+        <div className={classNames('popup', { show })}>
+          <span className="close" onClick={() => setIsShow(false)}>&times;</span>
            Network Error...
         </div>
     )
